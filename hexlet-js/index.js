b934fdc319e271d31f3836976dd1f2d4325a1606
Marina Echkina <marina-13_66@mail.ru>
2024-01-22 21:07:14 +0200

    New text

--- hexlet-js/index.js
+++ hexlet-js/index.js
@@ -1,2 +1,16 @@
-console.log('Hello, Hexlet')
+//console.log('Hello, Hexlet');
 
+
+import _ from "lodash";
+
+// Пересечение массивов
+console.log(_.intersection([2, 1], [2, 3])); // => [2]');
+
+// capitalize делает первую букву заглавной
+console.log(_.capitalize("hello")); // Hello
+
+
+
+import React from "react";
+
+console.log(_.last(['one', 'two']));
