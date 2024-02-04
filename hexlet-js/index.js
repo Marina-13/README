0969d1f7448f3ed14fefc8c0078b4ef006e2dc3e
Marina Echkina <marina-13_66@mail.ru>
2024-02-04 21:42:53 +0200

    adding linting

--- hexlet-js/index.js
+++ hexlet-js/index.js
@@ -1,16 +1,3 @@
-//console.log('Hello, Hexlet');
+import { random } from 'lodash';
 
-
-import _ from "lodash";
-
-// Пересечение массивов
-console.log(_.intersection([2, 1], [2, 3])); // => [2]');
-
-// capitalize делает первую букву заглавной
-console.log(_.capitalize("hello")); // Hello
-
-
-
-import React from "react";
-
-console.log(_.last(['one', 'two']));
+console.log(random(0, 5));
